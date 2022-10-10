4a5a5efcb809c8dad31296fa3297e57714f12f9c
Farid Hidayat <e.faridhidayat@gmail.com>
2022-10-11 01:01:53 +0700

    migrate: reactions migration

---
+++ database/migrations/2022_10_11_003143_create_reactions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reactions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->string('foreign_table')->nullable();
+            $table->string('foreign_id')->nullable();
+
+            $table->enum('type', ['like', 'love', 'star', 'dislike', 'other'])->default('like');
+            $table->string('reaction')->nullable();
+
+            $table->foreignUuid('created_by')->references('id')->on('users');
+            $table->foreignUuid('updated_by')->nullable()->references('id')->on('users');
+            $table->foreignUuid('deleted_by')->nullable()->references('id')->on('users');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reactions');
+    }
+};
